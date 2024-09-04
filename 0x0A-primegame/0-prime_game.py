5f260ac9a026ba145b00e357295ca7f3b6d86016
Abderrahmane Benjelloun <benjelloun857@gmail.com>
2024-09-04 21:16:27 +0000

    changes

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,48 +1,46 @@
 #!/usr/bin/python3
-"""Prime Game interview question"""
 
-
-def isprime(n):
-    """Check if prime number"""
-    for i in range(2, n):
+def is_prime(n):
+    """Check if n is a prime number."""
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    i = 3
+    while i * i <= n:
         if n % i == 0:
             return False
+        i += 2
     return True
 
-
-def delete_nums(n, nums):
-    """Remove numbers"""
-    for i in range(len(nums)):
-        if nums[i] % n == 0:
-            nums[i] = 0
-
-
 def isWinner(x, nums):
-    """ Return name of player that won
-    """
-    nums.sort()
-    winner = False
+    """Return the name of the player that won the most rounds."""
     Maria = 0
     Ben = 0
-    for game in range(x):
-        nums2 = list(range(1, nums[game] + 1))
+    
+    for n in nums:
+        if n == 1:
+            Ben += 1
+            continue
+        
+        primes = [i for i in range(2, n + 1) if is_prime(i)]
         turn = 0
-        while True:
-            change = False
-            for i, n in enumerate(nums2):
-                if n > 1 and isprime(n):
-                    delete_nums(n, nums2)
-                    change = True
-                    turn += 1
-                    break
-            if change is False:
-                break
+        
+        while primes:
+            current_prime = primes.pop(0)
+            primes = [p for p in primes if p % current_prime != 0]
+            turn += 1
+        
         if turn % 2 != 0:
             Maria += 1
         else:
             Ben += 1
-    if Maria == Ben:
-        return None
+    
     if Maria > Ben:
         return "Maria"
-    return "Ben"
+    elif Ben > Maria:
+        return "Ben"
+    else:
+        return None
