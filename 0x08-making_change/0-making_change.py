bd98539abaafbcce6d2fb4bc4910ef4eeec7861c
Abderrahmane Benjelloun <benjelloun857@gmail.com>
2024-09-19 20:13:12 +0000

    test v1.0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""making change challenge
+"""
+
+
+def makeChange(coins, total):
+    """Returns the number of coins needed to meet the total
+    """
+    if total <= 0:
+        return 0
+
+    if (coins is None or len(coins) == 0):
+        return -1
+
+    change = 0
+
+    avaib_coins = sorted(coins, reverse=True)
+
+    change_left = total
+
+    for coin in avaib_coins:
+        while (change_left % coin >= 0 and change_left >= coin):
+            change += int(change_left / coin)
+            change_left = change_left % coin
+
+    if change_left != 0:
+        change = -1
+
+    return change
